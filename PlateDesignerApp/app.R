f3ee284e36429edb82eca801421f27690a4bb377
ThijmenWeijgertze <129500989+ThijmenWeijgertze@users.noreply.github.com>
2024-11-10 00:09:57 +0100

    Added more parameters and support up to the max of 1536 wells

--- PlateDesignerApp/app.R
+++ PlateDesignerApp/app.R
@@ -3,17 +3,23 @@ library(readxl)
 library(bslib)
 library(ggplate)
 
-# Define UI ----
+# UI
 ui <- page_sidebar(
+  # UI title
   title = "Plate Designer",
+  # start sidebar
   sidebar = sidebar(
-    "Parameters", 
+    # sidebar title
+    "Parameters",
+    # sidebar position
     position = "left",
+    # sidebar .xlsx file input
     fileInput(
       "file1", 
       "Choose .xlsx File", 
-      accept = ".xlsx"),
-    selectInput( 
+      accept = ".xlsx"
+    ),
+    selectInput(
       "wellNumber", 
       "Select the plate type:", 
       list(
@@ -24,32 +30,44 @@ ui <- page_sidebar(
         "96 wells" = "96",
         "384 wells" = "384",
         "1536 wells" = "1536"
-        ) 
+      ) 
     ),
-    selectInput( 
+    input_switch(
+      "labelSwitch", "Labels", value = TRUE
+    ),
+    input_switch(
+      "labelLegend", "Legend", value = TRUE
+    ),
+    selectInput(
       "wellShape", 
       "Select the shape of the wells:", 
       list("round", "square") 
     ),
-    textInput( 
+    textInput(
       "titleField", 
       "Title", 
       placeholder = "Enter Title Here..."
+    ),
+    numericInput( 
+      "textSize", 
+      "Text Size", 
+      value = 23,
     ), 
+    downloadButton("downloadPlot", "Download Plot")
   ),
   card(
     card_header("Plate Design"),
-    plotOutput(outputId = "PlatePlot")
+    uiOutput(outputId = "plotOutputUI")
   )
 )
 
-# Define server logic ----
+# server
 server <- function(input, output) {
   
   # Reactive expression to read the file
   file_data <- reactive({
     req(input$file1)
-    read_excel(input$file1$datapath, sheet = paste0(input$wellNumber,"well"))
+    read_excel(input$file1$datapath, sheet = paste0(input$wellNumber, "well"))
   })
   
   output$PlatePlot <- renderPlot({
@@ -59,15 +77,70 @@ server <- function(input, output) {
       position = Well,
       value = Compound,
       label = Value,
+      show_legend = input$labelLegend,
       plate_size = input$wellNumber,
       plate_type = input$wellShape,
       title = input$titleField,
-      title_size = 23  
+      title_size = input$textSize
     )
   })
+  
+  output$PlatePlot2 <- renderPlot({
+    data <- file_data()
+    plate_plot(
+      data = data,
+      position = Well,
+      value = Compound,
+      show_legend = input$labelLegend,
+      plate_size = input$wellNumber,
+      plate_type = input$wellShape,
+      title = input$titleField,
+      title_size = input$textSize
+    )
+  })
+  
+  output$plotOutputUI <- renderUI({
+    if (input$labelSwitch) {
+      plotOutput(outputId = "PlatePlot")
+    } else {
+      plotOutput(outputId = "PlatePlot2")
+    }
+  })
+  
+  output$downloadPlot <- downloadHandler(
+    filename = function() {
+      paste("plate_plot", Sys.Date(), ".png", sep = "")
+    },
+    content = function(file) {
+      png(file, width = 1920, height = 1080, res = 300)
+      if (input$labelSwitch) {
+        print(plate_plot(
+          data = file_data(),
+          position = Well,
+          value = Compound,
+          label = Value,
+          show_legend = input$labelLegend,
+          plate_size = input$wellNumber,
+          plate_type = input$wellShape,
+          title = input$titleField,
+          title_size = input$textSize
+        ))
+      } else {
+        print(plate_plot(
+          data = file_data(),
+          position = Well,
+          value = Compound,
+          show_legend = input$labelLegend,
+          plate_size = input$wellNumber,
+          plate_type = input$wellShape,
+          title = input$titleField,
+          title_size = input$textSize
+        ))
+      }
+      dev.off()
+    }
+  )
 }
 
-# Run the app ----
+# Run
 shinyApp(ui = ui, server = server)
-
-
