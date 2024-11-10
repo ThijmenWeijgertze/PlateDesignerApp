b87b74f1eca86e7861b1a754bf92cee813b1bba0
ThijmenWeijgertze <129500989+ThijmenWeijgertze@users.noreply.github.com>
2024-11-10 03:14:25 +0100

    Added lot of parameters downloaded fixes and UI changes

--- PlateDesignerApp/app.R
+++ PlateDesignerApp/app.R
@@ -1,3 +1,4 @@
+# libraries
 library(shiny)
 library(readxl)
 library(bslib)
@@ -13,15 +14,16 @@ ui <- page_sidebar(
     "Parameters",
     # sidebar position
     position = "left",
-    # sidebar .xlsx file input
+    # browse excel file
     fileInput(
-      "file1", 
+      "excelFile", 
       "Choose .xlsx File", 
       accept = ".xlsx"
     ),
+    # choose well plate type
     selectInput(
       "wellNumber", 
-      "Select the plate type:", 
+      "Plate type", 
       list(
         "6 wells" = "6", 
         "12 wells" = "12", 
@@ -30,46 +32,122 @@ ui <- page_sidebar(
         "96 wells" = "96",
         "384 wells" = "384",
         "1536 wells" = "1536"
-      ) 
+      )
     ),
-    input_switch(
-      "labelSwitch", "Labels", value = TRUE
-    ),
-    input_switch(
-      "labelLegend", "Legend", value = TRUE
-    ),
+    # shape of the wells
     selectInput(
       "wellShape", 
-      "Select the shape of the wells:", 
+      "Shape", 
       list("round", "square") 
     ),
+    # title of the plot
     textInput(
       "titleField", 
       "Title", 
       placeholder = "Enter Title Here..."
     ),
+    # size of the title text
     numericInput( 
       "textSize", 
-      "Text Size", 
-      value = 23,
-    ), 
-    downloadButton("downloadPlot", "Download Plot")
+      "Title Size", 
+      value = 15
+    ),
+    # label size
+    numericInput( 
+      "labelSize", 
+      "Label Size", 
+      value = 2,
+      min = 0
+    ),
+    # legend rows
+    numericInput( 
+      "legendRows", 
+      "Number of legend rows", 
+      value = 3,
+      min = 1
+    ),
+    # seed for the randomizer
+    numericInput(
+      "randomSeed", 
+      "Randomizer Seed", 
+      value = 2024,
+      min = 1
+    ),
+    # switch to hide labels
+    input_switch(
+      "labelSwitch", 
+      "Labels",
+      value = TRUE
+    ),
+    # switch to hide legend
+    input_switch(
+      "labelLegend", 
+      "Legend", 
+      value = TRUE
+    ),
+    # switch to randomize
+    input_switch(
+      "randomSwitch", 
+      "Randomize",
+      value = FALSE
+    ),
+    input_dark_mode(id = "mode"),
+    # button to download the plot
+    downloadButton(
+      "downloadPlot", 
+      "Plot"
+    ),
+    # button to download the gradient plot
+    downloadButton(
+      "downloadPlotGradient", 
+      "Gradient Plot"
+    ),
+    # button to download the gradient plot
+    downloadButton(
+      "downloadPlotCompoundAsLabel", 
+      "Plot With Compound As Label"
+    )
   ),
-  card(
-    card_header("Plate Design"),
-    uiOutput(outputId = "plotOutputUI")
-  )
-)
+  # making the tabs
+  fluidPage(
+    tabsetPanel(
+      # Normal tab
+      tabPanel("Regular plate",
+        card(
+          card_header("Plate Design"),
+            uiOutput(outputId = "PlatePlotOutputUI")
+              )),
+      tabPanel("Gradient Plate",
+        card(
+          card_header("Plate Design Gradient"),
+            uiOutput(outputId = "PlatePlotGradientOutputUI")
+              )),
+      tabPanel("Plate With Compound As Label",
+        card(
+          card_header("Plate Design With Compound As Label"),
+            uiOutput(outputId = "PlatePlotCompoundAsLabelOutputUI")
+)))))
 
 # server
 server <- function(input, output) {
-  
-  # Reactive expression to read the file
+
+  # loading data
   file_data <- reactive({
-    req(input$file1)
-    read_excel(input$file1$datapath, sheet = paste0(input$wellNumber, "well"))
+    # loading in normal data
+    req(input$excelFile)
+    data <- read_excel(input$excelFile$datapath, sheet = paste0(input$wellNumber, "well"))
+    # randomize the data if switch is set to TRUE
+    if (input$randomSwitch) {
+      # set seed (if filled in)
+      set.seed(input$randomSeed)
+      # randomize column 2 and 3
+      data[, 2:3] <- data[sample(nrow(data)), 2:3]
+    }
+    # return the data
+    return(data)
   })
   
+  # rendering the normal plot with labels
   output$PlatePlot <- renderPlot({
     data <- file_data()
     plate_plot(
@@ -81,11 +159,14 @@ server <- function(input, output) {
       plate_size = input$wellNumber,
       plate_type = input$wellShape,
       title = input$titleField,
-      title_size = input$textSize
+      title_size = input$textSize,
+      legend_n_row = input$legendRows,
+      label_size = input$labelSize
     )
   })
   
-  output$PlatePlot2 <- renderPlot({
+  # rendering the normal plot without labels
+  output$PlatePlotNoLabel <- renderPlot({
     data <- file_data()
     plate_plot(
       data = data,
@@ -95,24 +176,96 @@ server <- function(input, output) {
       plate_size = input$wellNumber,
       plate_type = input$wellShape,
       title = input$titleField,
-      title_size = input$textSize
+      title_size = input$textSize,
+      legend_n_row = input$legendRows,
+      label_size = input$labelSize
     )
   })
   
-  output$plotOutputUI <- renderUI({
+  # rendering the normal plot with labels
+  output$PlatePlotGradient <- renderPlot({
+    data <- file_data()
+    plate_plot(
+      data = data,
+      position = Well,
+      value = Value,
+      label = Value,
+      show_legend = input$labelLegend,
+      plate_size = input$wellNumber,
+      plate_type = input$wellShape,
+      title = input$titleField,
+      title_size = input$textSize,
+      label_size = input$labelSize
+    )
+  })
+  
+  # rendering the normal plot without labels
+  output$PlatePlotNoLabelGradient <- renderPlot({
+    data <- file_data()
+    plate_plot(
+      data = data,
+      position = Well,
+      value = Value,
+      show_legend = input$labelLegend,
+      plate_size = input$wellNumber,
+      plate_type = input$wellShape,
+      title = input$titleField,
+      title_size = input$textSize,
+      label_size = input$labelSize
+    )
+  })
+  
+  # rendering the plot with the compound as label
+  output$PlatePlotCompoundAsLabel <- renderPlot({
+    data <- file_data()
+    plate_plot(
+      data = data,
+      position = Well,
+      value = Compound,
+      label = Compound,
+      show_legend = input$labelLegend,
+      plate_size = input$wellNumber,
+      plate_type = input$wellShape,
+      title = input$titleField,
+      title_size = input$textSize,
+      legend_n_row = input$legendRows,
+      label_size = input$labelSize
+    )
+  })
+  
+  # choosing which plot to show based on the label switch
+  output$PlatePlotOutputUI <- renderUI({
     if (input$labelSwitch) {
       plotOutput(outputId = "PlatePlot")
     } else {
-      plotOutput(outputId = "PlatePlot2")
+      plotOutput(outputId = "PlatePlotNoLabel")
     }
   })
   
+  # choosing which gradient plot to show based on the label switch
+  output$PlatePlotGradientOutputUI <- renderUI({
+    if (input$labelSwitch) {
+      plotOutput(outputId = "PlatePlotGradient")
+    } else {
+      plotOutput(outputId = "PlatePlotNoLabelGradient")
+    }
+  })
+  
+  output$PlatePlotCompoundAsLabelOutputUI <- renderUI({
+    plotOutput(outputId = "PlatePlotCompoundAsLabel")
+  })
+  
+  # downloading plot
   output$downloadPlot <- downloadHandler(
+    # PNG file name
     filename = function() {
-      paste("plate_plot", Sys.Date(), ".png", sep = "")
+      # (gsub removes any spaces)
+      paste(gsub(" ", "", input$titleField), Sys.Date(), ".png", sep = "")
     },
     content = function(file) {
-      png(file, width = 1920, height = 1080, res = 300)
+      # PNG properties (width, height, resolution)
+      png(file, width = 10, height = 6, unit = "in", res = 300)
+      # plot with labels
       if (input$labelSwitch) {
         print(plate_plot(
           data = file_data(),
@@ -123,8 +276,11 @@ server <- function(input, output) {
           plate_size = input$wellNumber,
           plate_type = input$wellShape,
           title = input$titleField,
-          title_size = input$textSize
+          title_size = input$textSize,
+          legend_n_row = input$legendRows,
+          label_size = input$labelSize
         ))
+      # plot without labels
       } else {
         print(plate_plot(
           data = file_data(),
@@ -134,13 +290,86 @@ server <- function(input, output) {
           plate_size = input$wellNumber,
           plate_type = input$wellShape,
           title = input$titleField,
-          title_size = input$textSize
+          title_size = input$textSize,
+          legend_n_row = input$legendRows,
+          label_size = input$labelSize
         ))
       }
+      # stop PNG render process
+      dev.off()
+    }
+  )
+  # downloading gradient plot
+  output$downloadPlotGradient <- downloadHandler(
+    # PNG file name
+    filename = function() {
+      # (gsub removes any spaces)
+      paste(gsub(" ", "", input$titleField), Sys.Date(), ".png", sep = "")
+    },
+    content = function(file) {
+      # PNG properties (width, height, resolution)
+      png(file, file, width = 10, height = 6, unit = "in", res = 300)
+      # plot gradient with labels
+      if (input$labelSwitch) {
+        print(plate_plot(
+          data = file_data(),
+          position = Well,
+          value = Value,
+          label = Value,
+          show_legend = input$labelLegend,
+          plate_size = input$wellNumber,
+          plate_type = input$wellShape,
+          title = input$titleField,
+          title_size = input$textSize,
+          label_size = input$labelSize
+        ))
+        # plot gradient without labels
+      } else {
+        print(plate_plot(
+          data = file_data(),
+          position = Well,
+          value = Value,
+          show_legend = input$labelLegend,
+          plate_size = input$wellNumber,
+          plate_type = input$wellShape,
+          title = input$titleField,
+          title_size = input$textSize,
+          label_size = input$labelSize
+        ))
+      }
+      # stop PNG render process
+      dev.off()
+    }
+  )
+  # downloading gradient plot
+  output$downloadPlotCompoundAsLabel <- downloadHandler(
+    # PNG file name
+    filename = function() {
+      # (gsub removes any spaces)
+      paste(gsub(" ", "", input$titleField), Sys.Date(), ".png", sep = "")
+    },
+    content = function(file) {
+      # PNG properties (width, height, resolution)
+      png(file, file, width = 10, height = 6, unit = "in", res = 300)
+      # plot with compound as label
+      print(plate_plot(
+        data = file_data(),
+        position = Well,
+        value = Compound,
+        label = Compound,
+        show_legend = input$labelLegend,
+        plate_size = input$wellNumber,
+        plate_type = input$wellShape,
+        title = input$titleField,
+        title_size = input$textSize,
+        legend_n_row = input$legendRows,
+        label_size = input$labelSize
+      ))
+      # stop PNG render process
       dev.off()
     }
   )
 }
 
-# Run
+# Run the app
 shinyApp(ui = ui, server = server)
